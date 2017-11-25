52ac94519a118a416f69c8b0daca738dbed02353
wuhaisheng <15624992448@163.com>
2017-11-25 17:01:04 +0800

    mybatis+Spring

--- src/main/java/com/niujiacun/music/common/utils/DynamicDataSource.java
+++ src/main/java/com/niujiacun/music/common/utils/DynamicDataSource.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  * Created by Administrator on 2017/11/25.
  */
 public class DynamicDataSource extends AbstractRoutingDataSource {
+
     @Override
     protected Object determineCurrentLookupKey() {
         return DataSourceContextHolder.getDbType();
